ed3564b1823c0d6c9c881e820e6d5548a1a427b7
ryofxbuilder <ryosuke_aka@yahoo.co.jp>
2016-12-20 23:03:36 +0900

    Initial Commit

---
+++ QRCodeReader/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRCodeReader
+//
+//  Created by Ryosuke Akanuma on 2016/12/11.
+//  Copyright © 2016年 jp.co.numa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+        //　RouteSummaryCellとJRPassをTableViewに追加するため
+
+}
+
